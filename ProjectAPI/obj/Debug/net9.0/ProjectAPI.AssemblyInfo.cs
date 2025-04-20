764e6f9319eaa2e9154d4907b0a119848d2a4f75
lindsey5 <lindseysamson5@gmail.com>
2025-04-20 14:49:09 +0800

    initial commit

--- ProjectAPI/obj/Debug/net9.0/ProjectAPI.AssemblyInfo.cs
+++ ProjectAPI/obj/Debug/net9.0/ProjectAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bfe33fd4cc3b0886109586abb6da6e6cb652e81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76d1d5c6f22cbc45db2d1022ee7b5098bde3b8a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
